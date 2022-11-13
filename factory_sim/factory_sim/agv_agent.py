7229b3cb882fec0b8213f357f01f94d60e66bdf9
jmotes <jmotes2@illinois.edu>
2022-11-13 17:12:42 -0600

    Updating agv logic.

--- factory_sim/factory_sim/agv_agent.py
+++ factory_sim/factory_sim/agv_agent.py
@@ -1,3 +1,5 @@
+import argparse
+
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSProfile, QoSReliabilityPolicy
@@ -7,9 +9,11 @@ from geometry_msgs.msg import PoseStamped
 from coordinated_commander.robot_navigator import NamespaceNavigator
 from nav2_simple_commander.robot_navigator import BasicNavigator
 
+from std_msgs.msg import String,Int32
+
 class AGVAgent(Node):
 
-  def __init__(self,namespace=''):
+  def __init__(self,namespace='',x=0,y=0):
     super().__init__('agv_agent')
 
     self.namespace = namespace
@@ -31,16 +35,39 @@ class AGVAgent(Node):
                                 durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
                                 reliability=QoSReliabilityPolicy.RELIABLE,
                                 history=QoSHistoryPolicy.KEEP_LAST,
-                                depth=1
+                                depth=10
                               )
                           )
 
+    #if self.namespace == '':
+    self.navigator = BasicNavigator()
+    #else:
+      #self.navigator = NamespaceNavigator(self.namespace)
 
-    if self.namespace == '':
-      self.navigator = BasicNavigator()
-    else:
-      self.navigator = NamespaceNavigator(self.namespace)
+    #msg = PoseStamped()
+    #msg.header.frame_id = 'map'
+    #msg.header.stamp = self.navigator.get_clock().now().to_msg()
+    #msg.pose.position.x = x
+    #msg.pose.position.y = y
+    #msg.pose.orientation.z = 1.0
+    #msg.pose.orientation.w = 0.0
 
+    #self.navigator.setInitialPose(msg)
+
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    #initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+    initial_pose.pose.position.x = x
+    initial_pose.pose.position.y = y
+    initial_pose.pose.orientation.z = 1.0
+    initial_pose.pose.orientation.w = 0.0
+
+    print('Setting initial pose',initial_pose)
+
+    self.navigator.setInitialPose(initial_pose)
+    self.navigator.waitUntilNav2Active()
+
+    print('Done initializing')
 
   def _addTaskCallback(self,task_pose):
     self.task_queue.append(task_pose)
@@ -49,12 +76,41 @@ class AGVAgent(Node):
       self.executeTasks()
 
   def executeTasks(self):
-      while self.goal_index < len(self.task_queue):
-        self.navigator.goToPose(self.task_queue[self.goal_index])
-        while not self.navigator.isTaskComplete():
-          print('Moving to task',self.task_queue[self.goal_index])
 
+      #rate = self.create_rate(1)
+
+      self.reached_goal = False
+
+      while self.goal_index < len(self.task_queue):
+        msg = PoseStamped()
+        msg.header.frame_id = 'map'
+        msg.pose = self.task_queue[self.goal_index].pose
+        self.navigator.goToPose(msg)
+
+        counter = 0
+        while not self.navigator.isTaskComplete():
+          counter = counter + 1
+          if counter % 100 == 0:
+            print(self.navigator.isTaskComplete())
+
+        self.complete_task(self.task_queue[self.goal_index])
         self.goal_index = self.goal_index + 1
+      
+      self.reached_goal = True
+
+  def complete_task(self,task):
+
+    print('COMPLETING TASK')
+
+    # grap topic from task
+    task_complete_pub = self.create_publisher(Int32,task.header.frame_id,1)
+
+    msg = Int32()
+    msg.data = 25
+
+    task_complete_pub.publish(msg)
+    self.get_logger().info('Publishing: "%i"' % msg.data)
+    return
     
 
 def main(args=None):
@@ -62,7 +118,27 @@ def main(args=None):
   
   rclpy.init(args=args)
 
-  agv_agent = AGVAgent()
+  parser = argparse.ArgumentParser(description='Read in filenames')
+  parser.add_argument('-x',help='Starting pose x value.')
+  parser.add_argument('-y',help='Starting pose y value.')
+  parser.add_argument('--name',help='Agent name.')
+  args = parser.parse_args()
+
+  x = args.x
+  if x != None:
+    x = float(x)
+
+  y = args.y
+  if y != None:
+    y = float(y)
+
+  name = args.name
+  if args.name == None:
+    name = ''
+
+  print(name,x,y)
+
+  agv_agent = AGVAgent(namespace=name,x=x,y=y)
 
   rclpy.spin(agv_agent)
 
--- factory_sim/factory_sim/dummy_allocator.py
+++ factory_sim/factory_sim/dummy_allocator.py
@@ -6,39 +6,41 @@ from geometry_msgs.msg import PoseStamped
 def main():
   rclpy.init()
 
+  robot1 = 'robot1'
+
   initial_pose = PoseStamped()
-  initial_pose.header.frame_id = 'map'
+  initial_pose.header.frame_id = 'origin'
   initial_pose.pose.position.x = 0.0
-  initial_pose.pose.position.y = 0.5
+  initial_pose.pose.position.y = 0.0
   initial_pose.pose.orientation.z = 1.0
   initial_pose.pose.orientation.w = 0.0
 
   
   pose1 = PoseStamped()
-  pose1.header.frame_id = 'map'
-  pose1.pose.position.x = 4.0
+  pose1.header.frame_id = 'take_from_depot'
+  pose1.pose.position.x = 8.0
   pose1.pose.position.y = 0.5
   pose1.pose.orientation.z = 1.0
   pose1.pose.orientation.w = 0.0
 
 
   pose2 = PoseStamped()
-  pose2.header.frame_id = 'map'
+  pose2.header.frame_id = 'ws0/receive_parts'
   pose2.pose.position.x = 4.0
-  pose2.pose.position.y = 2.5
+  pose2.pose.position.y = 3.0
   pose2.pose.orientation.z = 1.0
   pose2.pose.orientation.w = 0.0
 
 
   pose3 = PoseStamped()
-  pose3.header.frame_id = 'map'
+  pose3.header.frame_id = 'ws3/receive_parts'
   pose3.pose.position.x = -4.0
-  pose3.pose.position.y = 0.5
+  pose3.pose.position.y = -3.0
   pose3.pose.orientation.z = 1.0
   pose3.pose.orientation.w = 0.0
 
   node = Node('dummy_allocator')
-  pub = node.create_publisher(PoseStamped,'task_queue',
+  pub = node.create_publisher(PoseStamped,robot1 + '/task_queue',
 
                               QoSProfile(
                                 durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
